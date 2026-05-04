d6c253963ff31390748488c5d91466b5f48d48a0
Khushi Gupta <khushi.dims1803@gmail.com>
2026-05-04 13:41:22 +0530

    Force migrations in build script

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
+
+# This is the line that will fix your error
+python manage.py makemigrations
 python manage.py migrate  # This now migrates to PostgreSQL
